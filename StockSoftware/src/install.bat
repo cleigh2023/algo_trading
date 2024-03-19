7dce0328850fff391deaa288a2057b474510d9e1
cleigh2023 <163941159+cleigh2023@users.noreply.github.com>
2024-03-19 12:41:36 -0500

    Delete StockSoftware/src directory

--- StockSoftware/src/install.bat
+++
@@ -1,9 +0,0 @@
-.\python-3.11.8-amd64.exe InstallAllUsers=1 PrependPath=1 Include_test=0
-setx /M path "%path%;C:\Program Files\Python311"
-$env:PATH =$env:PATH+";C:\Program Files\Python311"
-python3 -m ensurepip --upgrade
-pip3 install alpaca-py
-pip3 install datetime
-pip3 install requests
-pip3 install pandas
-pip3 install numpy
--- StockSoftware/src/main.py
+++
@@ -1,8 +0,0 @@
-from alpaca_data import AlpacaTrade
-
-def main():
-    ALPACA = AlpacaTrade(isPaper=False)
-    ALPACA.verify_all_symbols(check_sameday=False)
-
-if __name__ == '__main__':
-    main()
